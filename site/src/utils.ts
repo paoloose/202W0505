19bf16f264c190bbedebc01a115a36a8986bde32
Paolo Flores <pflores.fisi22@gmail.com>
2024-04-30 12:16:24 -0500

    fix urls

--- site/src/utils.ts
+++ site/src/utils.ts
@@ -1,9 +1,26 @@
+type UrlCheckConf = {
+  prodOnly: boolean;
+};
 
-export async function ensureUrlReturns200(url: string) {
+export async function ensureUrlReturns200(url: string, conf?: UrlCheckConf) {
+  if (conf?.prodOnly && process.env.NODE_ENV !== 'production') {
+    console.log(`Skipping URL check for ${url} in non-production environment`);
+    return;
+  }
+
   const response = await fetch(url);
   if (response.status !== 200) {
     console.error(`Error fetching ${url}: ${response.statusText}`);
-    throw new Error();
+    throw new Error(`Error fetching ${url}: ${response.statusText}`);
+  }
+}
+
+export async function returns200(url: string, conf?: UrlCheckConf): Promise<boolean> {
+  try {
+    await ensureUrlReturns200(url, conf);
+    return true;
+  } catch (e) {
+    return false;
   }
 }
 
